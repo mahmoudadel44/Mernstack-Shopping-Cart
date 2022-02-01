b9afed348bc0fe3928499a584ec420ab8e4f14c6
mahmoudadel44 <mrmahmoudadel1996@gmail.com>
2022-02-01 04:56:42 +0200

    adding-Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Routes } from "react-router-dom";
 
+import Navbar from "./components/Navbar/Navbar";
 function App() {
   return (
     <Router>
       <div>
         <header>
+          <Navbar />
         </header>
       </div>
     </Router>
