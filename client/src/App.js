a0edc2eb6c92098975d06307a57f818e987732fa
mahmoudadel44 <mrmahmoudadel1996@gmail.com>
2022-02-01 04:55:27 +0200

    create-mernstack-shoppingg-cart

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,10 @@
 import { BrowserRouter as Router, Routes } from "react-router-dom";
 
-import Navbar from "./components/Navbar/Navbar";
 function App() {
   return (
     <Router>
       <div>
         <header>
-          <Navbar />
         </header>
       </div>
     </Router>
