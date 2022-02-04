e9d411d664887f3b7934f9818ddea7e22b310611
mahmoudadel44 <mrmahmoudadel1996@gmail.com>
2022-02-04 03:09:35 +0200

    adding-products-filters

--- client/src/components/Products/Products.js
+++ client/src/components/Products/Products.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import ProductData from "../../data/productdata.json";
 import ProductCard from "../Cards/ProductCard";
+import Filters from "../Filters/Filters";
 
 const Products = () => {
   const [productData, setProductData] = useState(ProductData);
@@ -11,7 +12,7 @@ const Products = () => {
           <h5 className="my-3 title">Latest Products</h5>
 
           <div className="row text-center">
-            <div className="col-md-9">
+            <div className="col-lg-8 col-md-7">
               <div className="row">
                 {productData &&
                   productData.map((product) => (
@@ -19,8 +20,8 @@ const Products = () => {
                   ))}
               </div>
             </div>
-            <div className="col-md-3">
-              Filters
+            <div className="col-lg-4 col-md-5">
+              <Filters />
             </div>
           </div>
         </div>
